a35cf8b6d2a4aa47024107f9a3cd6821671276fd
Tiomuch <artimerz@ukr.net>
2023-05-08 18:27:39 +0300

    Add login, profile, reset password routes

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -5,23 +5,28 @@ import jwt from 'jsonwebtoken'
 
 type User = {
   id: number
-  name: string
+  username: string
   password: string
   secret_word: string
 }
 
-type UserRequestBody = {
-  name: string
+type UserRegisterBody = {
+  username: string
   password: string
   secret_word: string
+}
+
+type UserAuthBody = {
+  username: string
+  password: string
 }
 
 export const register = async (req: Request, res: Response): Promise<void> => {
-  const { name, password, secret_word } = req.body as UserRequestBody
+  const { username, password, secret_word } = req.body as UserRegisterBody
 
-  const user = await db.query<User>('SELECT * from users where name = $1', [name])
+  const users = await db.query<User>('SELECT * from users where username = $1', [username])
 
-  if (!!user?.rows[0]) {
+  if (!!users?.rows[0]) {
     res.status(400).json({
       message: 'Name already in use',
     })
@@ -34,18 +39,103 @@ export const register = async (req: Request, res: Response): Promise<void> => {
   const hashedSecretWord = bcrypt.hashSync(secret_word, salt)
 
   const newPerson = await db.query<User>(
-    'INSERT INTO users (name, password, secret_word) values ($1, $2, $3) returning *',
-    [name, hashedPassword, hashedSecretWord],
+    'INSERT INTO users (username, password, secret_word) values ($1, $2, $3) returning *',
+    [username, hashedPassword, hashedSecretWord],
   )
   const newUser = newPerson.rows[0]
 
-  const token = jwt.sign({ id: newUser.id, name }, process.env.TOKEN_KEY as string, {
+  const token = jwt.sign({ id: newUser.id, username }, process.env.TOKEN_KEY || '', {
     expiresIn: '1h',
   })
 
   res.status(200).json({ data: newUser, token })
 }
 
-export const login = (req: Request, res: Response) => {
-  // TODO Add logic to user login
+export const login = async (req: Request, res: Response): Promise<void> => {
+  const { username, password } = req.body as UserAuthBody
+  const users = await db.query<User>('SELECT * from users where username = $1', [username])
+
+  if (!users?.rows[0]) {
+    res.status(400).json({
+      message: 'User does not exist',
+    })
+
+    return
+  }
+
+  const passwordCorrect = bcrypt.compareSync(password, users?.rows[0]?.password)
+
+  if (!passwordCorrect) {
+    res.status(400).json({
+      message: 'Password is not correct',
+    })
+
+    return
+  }
+
+  const user = users.rows[0]
+
+  const token = jwt.sign({ id: user.id, username }, process.env.TOKEN_KEY || '', {
+    expiresIn: '1h',
+  })
+
+  res.status(200).json({ data: user, token })
+}
+
+export const profile = async (req: Request, res: Response): Promise<void> => {
+  const token = req.headers['authorization'] as string
+  const user = jwt.decode(token?.split(' ')[1]) as User | null
+
+  if (!user) {
+    res.status(400).json({
+      message: 'Invalid token',
+    })
+
+    return
+  }
+
+  const dbUser = await db.query('SELECT * from users where username = $1', [user.username])
+
+  if (!dbUser?.rows[0]) {
+    res.status(400).json({
+      message: 'User does not exist',
+    })
+
+    return
+  }
+
+  res.status(200).json(dbUser?.rows[0])
+}
+
+export const passwordReset = async (req: Request, res: Response): Promise<void> => {
+  const { username, password, secret_word } = req.body as UserRegisterBody
+
+  const users = await db.query('SELECT * from users where username = $1', [username])
+
+  if (!users?.rows[0]) {
+    res.status(400).json({
+      message: 'User does not exist',
+    })
+
+    return
+  }
+
+  const secretWordCorrect = bcrypt.compareSync(secret_word, users?.rows[0]?.secret_word)
+
+  if (!secretWordCorrect) {
+    res.status(400).json({
+      message: 'Secret code is not correct',
+    })
+
+    return
+  }
+
+  const salt = bcrypt.genSaltSync(10)
+  const hashedPassword = bcrypt.hashSync(password, salt)
+
+  await db.query('UPDATE users set password = $1 where username = $2 returning *', [hashedPassword, username])
+
+  res.status(200).json({
+    message: 'Password changed successfully',
+  })
 }
--- src/routes/auth.router.ts
+++ src/routes/auth.router.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express'
-import { login, register } from '../controllers/auth.controller.js'
+import { login, register, profile, passwordReset } from '../controllers/auth.controller.js'
+import verifyToken from '../middlewares/auth.js'
 
 const router = Router()
 
 router.post('/register', register)
 router.post('/login', login)
+router.post('/password-reset', passwordReset)
+router.get('/profile', verifyToken, profile)
 
 export default router
