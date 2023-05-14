8894721901ab12bbd5c6a584f3c0e064e5d7a4a8
Tiomuch <artimerz@ukr.net>
2023-05-15 01:51:44 +0300

    Update get posts

--- src/controllers/posts.controller.ts
+++ src/controllers/posts.controller.ts
@@ -40,11 +40,38 @@ export const createPost = async (req: Request, res: Response): Promise<void> => 
 
 export const getPosts = async (req: Request, res: Response): Promise<void> => {
   try {
-    const { limit = 10 } = req.query
+    const { limit = 10, title = '', sort_variant = 'ASC', category_id = null } = req.query
 
-    const posts = await db.query('SELECT * from posts limit $1', [limit])
+    const token = req.headers['authorization'] as string
+    const user = jwt.decode(token?.split(' ')[1]) as User | null
 
-    const total = await db.query('SELECT COUNT(*) from posts')
+    let query = `SELECT p.*, 
+        COALESCE(SUM(CASE WHEN l.liked THEN 1 ELSE 0 END), 0) as likes_count,
+        COALESCE(SUM(CASE WHEN NOT l.liked THEN 1 ELSE 0 END), 0) as dislikes_count,
+        MAX(CASE WHEN l.user_id = $1 THEN CAST(l.liked AS INT) ELSE NULL END) as liked
+      FROM posts p 
+      LEFT JOIN likes_for_posts l ON p.id = l.post_id 
+      WHERE p.title ILIKE $2 || '%'`
+
+    const params = [user?.id, title]
+
+    if (category_id) {
+      query += ` AND p.category_id = $${params.length + 1}`
+      params.push(category_id)
+    }
+
+    query += ` GROUP BY p.id 
+      ORDER BY p.created_at ${sort_variant}
+      LIMIT $${params.length + 1}`
+
+    params.push(limit)
+
+    const posts = await db.query(query, params)
+
+    const total = await db.query(
+      `SELECT COUNT(*) from posts WHERE title ILIKE $1 || '%' ${category_id ? 'AND category_id = $2' : ''}`,
+      category_id ? [title, category_id] : [title],
+    )
 
     res.status(200).json({ data: posts.rows, total: +total.rows[0].count })
   } catch (error) {
