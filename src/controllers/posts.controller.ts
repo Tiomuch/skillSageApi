ddacf02686263c7b9cb4d538dbf1605f3ff154c2
Tiomuch <artimerz@ukr.net>
2023-05-17 15:14:01 +0300

    Add likes in getPostById

--- src/controllers/posts.controller.ts
+++ src/controllers/posts.controller.ts
@@ -85,8 +85,22 @@ export const getPosts = async (req: Request, res: Response): Promise<void> => {
 export const getPostById = async (req: Request, res: Response): Promise<void> => {
   try {
     const id = req.params.id
+    const token = req.headers['authorization'] as string
+    const user = jwt.decode(token?.split(' ')[1]) as User | null
 
-    const posts = await db.query('SELECT * from posts WHERE id = $1', [id])
+    const posts = await db.query(
+      `
+      SELECT p.*,
+        COALESCE(SUM(CASE WHEN l.liked THEN 1 ELSE 0 END), 0) as likes_count,
+        COALESCE(SUM(CASE WHEN NOT l.liked THEN 1 ELSE 0 END), 0) as dislikes_count,
+        MAX(CASE WHEN l.user_id = $2 THEN CAST(l.liked AS INT) ELSE NULL END) as liked
+      FROM posts p
+      LEFT JOIN likes_for_posts l ON p.id = l.post_id
+      WHERE p.id = $1
+      GROUP BY p.id
+    `,
+      [id, user?.id],
+    )
 
     if (!posts?.rows[0]) {
       res.status(400).json({
